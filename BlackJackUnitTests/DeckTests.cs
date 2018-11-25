71ff02e6e5ed73cf4a96002a8c1d46fda9cf447c
Alexei Mostovoi <1796186@johnabbottcollege.net>
2018-11-25 12:23:05 -0500

    Add classes PlayerTest and Deck Tests

---
+++ BlackJackUnitTests/DeckTests.cs
@@ -0,0 +1,33 @@
+﻿using BlackJackProject.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackUnitTests
+{
+    [TestClass]
+    public class DeckTests
+    {
+        [TestMethod]
+        public void TestBuldDeck()
+        {
+            Deck deck = new Deck();
+
+
+            deck.Builddeck();
+
+            // Test total number of cards in new created deck
+            Assert.AreEqual(52, deck.DeckList.Count);
+
+            CardPlayer cardPlayer = new CardPlayer();
+
+            cardPlayer.Hand = deck.DeckList;
+            cardPlayer.getHandValue();
+
+            // Test maximum number of points that can appear in any blackjack game
+            Assert.AreEqual(340, cardPlayer.HandValue);
+
+        }
+    }
+}
